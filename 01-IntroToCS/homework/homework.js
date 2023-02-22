7d872de9d233c3f901c41eb40f2e3d30a66555a9
ManuelE <apamanuel@gmail.com>
2023-02-22 14:39:26 -0400

    Homework 1

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,8 +1,23 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+   var array = num.split('').reverse();
+   var suma = 0;
+   for (var i = 0; i < array.length; i++){
+      suma = suma + (array[i] * Math.pow(2,i));
+   };
+   return suma;
+}
 
-function DecimalABinario(num) {}
+function DecimalABinario(num) {
+   var array =[];
+   var div = num;
+   for (var i = 0; div > 0; i++){
+      array[i] = div % 2;
+      div = Math.floor(div / 2);
+   };
+   return array.reverse().join('');   
+}
 
 module.exports = {
    BinarioADecimal,
