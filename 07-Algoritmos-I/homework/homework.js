4596a60e1e838b14532b63f17a07aa695040e940
ManuelE <apamanuel@gmail.com>
2023-03-20 22:53:18 -0400

    Solucion homework 07

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -6,7 +6,17 @@ function factorear(num) {
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
-
+  let divisor = 2;
+  let array = [];
+  while (num !== 1){
+    while(num % divisor === 0){
+      num = num / divisor; 
+      array.push(divisor);     
+    };
+    divisor++;
+  };
+  array.unshift(1);
+  return array;
 }
 
 function bubbleSort(array) {
@@ -14,7 +24,21 @@ function bubbleSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  let arrayCopy = array.slice();
+  let swap = true;
+  let aux;
+  while(swap === true){
+    swap = false;
+    for (let j = 0; j < array.length-1; j++){
+      if (arrayCopy[j] > arrayCopy[j+1]){
+        aux = arrayCopy[j];
+        arrayCopy[j] = arrayCopy[j+1];
+        arrayCopy[j+1] = aux;
+        swap = true;
+      };
+    };
+  };
+  return arrayCopy;
 }
 
 
@@ -23,7 +47,20 @@ function insertionSort(array) {
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  let newArray = array;
+  let muestra;
+  let comparador;
+  for (let i = 0; i < array.length-1; i++){
+    for(let j = i + 1; j > 0; j--){
+      muestra = array[j];
+      comparador = array[j-1];
+      if(muestra < comparador){
+        newArray[j-1] = muestra;
+        newArray[j] = comparador;
+      };      
+    };    
+  };
+  return newArray;
 }
 
 
@@ -32,7 +69,23 @@ function selectionSort(array) {
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  let arrayNew = []; //es la matriz resultante
+  let arrayCopy = array.slice(); //esta es la matriz que voy a ir modificando
+  let index; // este va a ser el indice que necesitare para ir eliminando minimo de la matriz arrayCopy
+  let minimo; // es el valor minimo de la busqueda
+  for (let i = 0; i < array.length; i++){
+    minimo = arrayCopy[0];
+    index = 0;
+    for(let j = 0; j < arrayCopy.length; j++){
+      if(minimo > arrayCopy[j]){
+        minimo = arrayCopy[j];
+        index = j;
+      };
+    };
+    arrayNew.push(minimo);
+    arrayCopy.splice(index,1);    
+  };
+  return arrayNew;
 }
 
 
